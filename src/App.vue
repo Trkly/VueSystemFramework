6d5a6f2f58678edfe178c4391c7fa277b8cdc68a
zzk <Acapplella@163.com>
2024-01-26 00:12:04 +0800

    chore: 配置项目的Eslint,Prettier,Commitizen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <nav><router-link to="/">Home</router-link> | <router-link to="/about">About</router-link></nav>
+  <router-view />
 </template>
+
+<script setup>
+const a = ref(null)
+</script>
 
 <style lang="scss">
 #app {
